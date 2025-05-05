a984cb021b51907794d912c1be52e3baac129021
divy <arpitdivy@gmail.com>
2025-05-05 10:16:54 +0530

    1 qus

---
+++ Leetcode/March/solutions/790-Domino-and-Tromino-Tiling.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/domino-and-tromino-tiling/?envType=daily-question&envId=2025-05-05
+class Solution {
+    private static final int mod = 1_000_000_007;
+    public int numTilings(int n) {
+        if (n <= 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 5;
+        int[] dp = new int[n+1];
+        dp[0] = 1; dp[1] = 1; dp[2] = 2; dp[3] = 5;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (int)((2L * dp[i-1] + dp[i-3]) % mod);
+        }
+        return dp[n];
+    }
+}
