0532ff1f7763567a6ccf9c184d5ef52f136137a0
divy <arpitdivy@gmail.com>
2026-05-03 15:17:55 +0530

    more sql qus

--- Leetcode/solutions/solutions/1075-Project-Employees-I.sql
+++ Leetcode/solutions/solutions/1075-Project-Employees-I.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-SELECT PT.PROJECT_ID AS project_id,SUM(EXPERIENCE_YEARS)/ COUNT(PT.EMPLOYEE_ID) as average_years FROM EMPLOYEE ET  JOIN PROJECT PT ON ET.EMPLOYEE_ID = PT.EMPLOYEE_ID GROUP BY PT.PROJECT_ID  ;
+SELECT PT.PROJECT_ID AS project_id,ROUND(SUM(EXPERIENCE_YEARS)/ COUNT(PT.EMPLOYEE_ID),2) as average_years FROM EMPLOYEE ET  JOIN PROJECT PT ON ET.EMPLOYEE_ID = PT.EMPLOYEE_ID GROUP BY PT.PROJECT_ID  ;
+
+
+-- | PROJECT_ID | GROUP_CONCAT(PT.EMPLOYEE_ID) |
+-- | ---------- | ---------------------------- |
+-- | 1          | 1,2,3                        |
+-- | 2          | 1,4                          |
